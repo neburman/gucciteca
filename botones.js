1aada44925b7da287e2cf825dbec9bc70d0be246
neburman <ruben.moreno1204@gmail.com>
2021-01-30 19:04:24 +0100

    Proyecto

---
+++ botones.js
@@ -0,0 +1,9 @@
+function boton1(){
+    document.getElementById("img").src="imagenes/imagen1.jpg";
+}
+function boton2(){
+    document.getElementById("img").src="imagenes/imagen2.jpg";
+}
+function boton3(){
+    document.getElementById("img").src="imagenes/imagen3.jpg";
+}
