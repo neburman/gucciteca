3498b5ceb0c70b27aa71930ea75e8ed984dad522
neburman <ruben.moreno1204@gmail.com>
2021-05-15 00:39:26 +0200

    Proyecto

--- administracion/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $basededatos='2021Equipo5';
-    $usuario='2021Equipo5';
-    $server='datos.somorrostro.com';
-    $contrasena='manolcabron';
-    $conectar=mysqli_connect($server,$usuario,$contrasena,$basededatos);
-?>
